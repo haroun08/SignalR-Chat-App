c0b975e6f6908dd6ef98a28a31609020b3ec72df
haroun08 <129984358+haroun08@users.noreply.github.com>
2025-02-06 15:29:11 +0100

    Message Streamed to all users

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string message,string user)
+        {
+            Clients.All.SendAsync("ReceiveMessage",message, user);
+        }
+    }
+}
